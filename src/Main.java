d8cc2a61995e030e9488cec27117006afa8d722e
Anubys <36929031+DechevBoris@users.noreply.github.com>
2018-10-23 16:28:32 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BullsAndCows app = new BullsAndCows();
+        app.createAppForm();
+        app.setVisible(true);
+    }
+}
